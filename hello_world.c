72459dcbdf63f9bb188a41b69d1cea5ce388ad74
ENG19CS0042-ARPITKUMAR <76117980+ENG19CS0042-ARPIT@users.noreply.github.com>
2021-03-15 20:08:48 +0530

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,1 +1,10 @@
 //Write the program to print "Hello World !"
+#include<stdio.h>
+int  main ()
+{
+    char c[40];
+    printf("Enter your name\n");
+    scanf("%s",&c);
+    printf("%s is the name\n",c);
+return 0;
+}
