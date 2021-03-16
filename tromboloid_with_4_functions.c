cc5841a7756a33f6f6fdbc2a0b069a0e2f6bae5a
ENG19CS0042-ARPITKUMAR <76117980+ENG19CS0042-ARPIT@users.noreply.github.com>
2021-03-16 10:38:02 +0530

    Update tromboloid_with_4_functions.c

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -1,1 +1,28 @@
 //WAP to find the volume of a tromboloid using 4 functions.
+#include<stdio.h>
+int h, b, d;
+void input()
+{
+    printf("Enter the values of h, b and d respectively: "); 
+    scanf("%d%d%d", &h, &b, &d);
+}
+float volume(int h, int b, int d)
+{
+    float vol;
+    float x, y;
+    x = h*b*d; y = d/b;
+    vol = (x+y)*1/3;
+    return vol;
+}
+void disp()
+{
+    float v;
+    v = volume(h,b,d);
+    printf("The volume of the given troboloid with h: %d, b: %d and d: %d is %f cube units\n", h,b,d,v);
+}
+int main()
+{
+    input();
+    disp();
+    return 0;
+}
