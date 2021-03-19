8aff2bc038c8b4deabf79d259deb7ffa1508fc3d
ENG19CS0042-ARPITKUMAR <76117980+ENG19CS0042-ARPIT@users.noreply.github.com>
2021-03-19 10:47:28 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,1 +1,16 @@
 //WAP to find the sum of n fractions.
+#include<stdio.h>
+int main()
+{
+    int n, sum = 0, i, a[100];
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    printf("Enter the numbers: ");
+    for(i = 1; i<=n; i++)
+    {
+scanf("%d", &a[i]);
+        sum+= a[i];
+    }
+    printf("Sum of these %d numbers is %d",n,sum);
+    return 0;
+}
